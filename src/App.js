9b375a14aacb2f7929db01cb53b064084e8808af
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-07-21 17:08:57 +0630

    learnt about State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet';
-import Welcome from './components/Welcome';
+// import { Greet } from './components/Greet';
+// import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Takao">
-        <p>This is a child.</p>   
-      </Greet>
-      <Greet name="Aung" />
-      <Greet name="Shifu" />
-      { /* <Welcome /> */ }
+      <Message />
     </div>
   );
 }
