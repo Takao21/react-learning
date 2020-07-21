e24837e43e8845ef3bd3cfb8e95e5b8f3b47d50c
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-07-22 02:03:13 +0630

    it was good, git in atom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { Greet } from './components/Greet';
+// import { Greet } from './components/Greet.js';
 // import Welcome from './components/Welcome';
 import Message from './components/Message';
 
