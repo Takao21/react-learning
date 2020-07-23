ab42befcbebe78741d408ed40890bdcf52f64f30
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-07-23 19:17:12 +0630

    react lesson 13, event handling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Greet } from "./components/Greet.js";
-import Welcome from "./components/Welcome";
+import { isCompositeComponent } from "react-dom/test-utils";
+// import { Greet } from "./components/Greet.js";
+// import Welcome from "./components/Welcome";
 // import Message from "./components/Message";
 // import Count from "./components/Count.js";
+import FunctionClick from "./components/FunctionClick.js";
+import ClassClick from "./components/ClassClick";
 
 function App() {
   return (
     <div className="App">
       {/* <Count /> */}
-      <Greet name="Bruce" heroName="Batman" />
-      <Welcome name="Aung" heroName="Takao" />
+      {/* <Greet name="Bruce" heroName="Batman" />
+      <Welcome name="Aung" heroName="Takao" /> */}
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
